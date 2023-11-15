d74b9385229a3812744ee33724838983c1f639f0
Grey Owl <grey4owl@gmail.com>
2023-11-15 22:00:16 +0100

    Update

--- htmx-preserve-attr.js
+++ htmx-preserve-attr.js
@@ -4,18 +4,25 @@ htmx.defineExtension("preserve-attr", {
   onEvent : function(name, evt) {
     if (name === "htmx:afterSwap") {
       let target = evt.detail.target
-      Array.from(target.attributes).map((response) => {
-        let attr_name = response.name
-        let attr_value = response.value
-        let swap_type = target.getAttribute("hx-swap")
-        if (attr_name.startsWith("hx:") && swap_type === "outerHTML") {
+      let attributes = Array.from(target.attributes)
+      let swap_type = target.getAttribute("hx-swap")
+      let filter = attributes.filter((attribute) => {
+        return attribute.name.startsWith("hx:")
+      })
+      if (swap_type === "outerHTML") {
+        filter.map((response) => {
+          let attr_name = response.name
+          let attr_value = response.value
+          target = evt.detail.elt
+          attr_name = attr_name.replace("hx:", "")
+          target.setAttribute(attr_name, attr_value)
           htmx.on(evt.target, "htmx:load", (evt) => {
             target = evt.detail.elt
             attr_name = attr_name.replace("hx:", "")
             target.setAttribute(attr_name, attr_value)
           })
-        }
-      })
+        })
+      }
     }
   }
 })
