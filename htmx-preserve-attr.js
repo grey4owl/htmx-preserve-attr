1647854264ab7f7eab900508cdd818344dc65a46
Grey Owl <grey4owl@gmail.com>
2023-11-16 18:38:19 +0100

    Update

--- htmx-preserve-attr.js
+++ htmx-preserve-attr.js
@@ -2,25 +2,23 @@
 //--  created by: maᴚko.  
 htmx.defineExtension("preserve-attr", {
   onEvent : function(name, evt) {
-    if (name === "htmx:afterSwap") {
-      let target = evt.detail.target
-      let attributes = Array.from(target.attributes)
-      let swap_type = target.getAttribute("hx-swap")
-      let filter = attributes.filter((attribute) => {
-        return attribute.name.startsWith("hx:")
-      })
-      if (swap_type === "outerHTML") {
+    if (name === "htmx:afterOnLoad") {
+      let target = evt.detail.requestConfig.target
+      let swap_type = target.attributes["hx-swap"] ? target.attributes["hx-swap"].value : false
+      let init_hash = evt.detail.requestConfig.target["htmx-internal-data"].initHash
+      if (swap_type === "outerHTML" && init_hash === null) {
+        let get_attributes = Array.from(target.attributes)
+        let filter = get_attributes.filter((attribute) => {
+          return attribute.name.startsWith("hx:")
+        })
+        let new_attributes = evt.detail.requestConfig.target["htmx-internal-data"].replacedWith["attributes"]
         filter.map((response) => {
           let attr_name = response.name
-          let attr_value = response.value
-          target = evt.detail.elt
           attr_name = attr_name.replace("hx:", "")
-          target.setAttribute(attr_name, attr_value)
-          htmx.on(evt.target, "htmx:load", (evt) => {
-            target = evt.detail.elt
-            attr_name = attr_name.replace("hx:", "")
-            target.setAttribute(attr_name, attr_value)
-          })
+          let attr_value = response.value
+          let new_attr = document.createAttribute(attr_name)
+          new_attr.value = attr_value
+          new_attributes.setNamedItem(new_attr)
         })
       }
     }
