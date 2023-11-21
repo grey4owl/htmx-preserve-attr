2a913fef9891f3e523e17434ca01b30ba7803bc3
Grey Owl <grey4owl@gmail.com>
2023-11-21 11:55:33 +0100

    Update

--- htmx-preserve-attr.js
+++ htmx-preserve-attr.js
@@ -2,16 +2,20 @@
 //--  created by: maᴚko.  
 htmx.defineExtension("preserve-attr", {
   onEvent : function(name, evt) {
+    /* if (name === "htmx:beforeOnLoad") {
+      let new_attributes = evt.detail.target["htmx-internal-data"]
+      console.log(new_attributes)
+    } */
     if (name === "htmx:afterOnLoad") {
-      let target = evt.detail.requestConfig.target
+      let target = evt.detail.target
       let swap_type = target.attributes["hx-swap"] ? target.attributes["hx-swap"].value : false
-      let init_hash = evt.detail.requestConfig.target["htmx-internal-data"].initHash
+      let init_hash = target["htmx-internal-data"].initHash
       if (swap_type === "outerHTML" && init_hash === null) {
         let get_attributes = Array.from(target.attributes)
         let filter = get_attributes.filter((attribute) => {
           return attribute.name.startsWith("hx:")
         })
-        let new_attributes = evt.detail.requestConfig.target["htmx-internal-data"].replacedWith["attributes"]
+        let new_attributes = target["htmx-internal-data"].replacedWith["attributes"]
         filter.map((response) => {
           let attr_name = response.name
           attr_name = attr_name.replace("hx:", "")
